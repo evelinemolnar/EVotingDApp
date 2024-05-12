2d96450469d356ba51355efda0201778073c328d
evelinemolnar <noemi.eveline.molnar@stud.ubbcluj.ro>
2024-05-12 14:58:44 +0300

    style update

--- src/loadElections.js
+++ src/loadElections.js
@@ -1,71 +1,83 @@
-import {Default} from './loadBlockchain.js';
-import {Election} from './Election.js';
+import { Default } from "./loadBlockchain.js";
+import { Election } from "./Election.js";
 
 export var ElectionData = {
   // get() is a loader function, to run loadElections() function.
   get: async () => {
-      await ElectionData.loadElections();
-      await ElectionData.loadElectionDetails();
+    await ElectionData.loadElections();
+    await ElectionData.loadElectionDetails();
   },
 
   // Loading deployed election contracts in Default.election array
   loadElections: async () => {
-      ElectionData.elections = [];
-      ElectionData.electionCount = await Default.MainContract.electionId();
-      for(var i = 0; i < ElectionData.electionCount; i++) {
-          var electionAddress = await Default.MainContract.Elections(i);
-          var election = await new Election(electionAddress);
-          await election.init();
-          ElectionData.elections.push(election);
-      }
-
+    ElectionData.elections = [];
+    ElectionData.electionCount = await Default.MainContract.electionId();
+    for (var i = 0; i < ElectionData.electionCount; i++) {
+      var electionAddress = await Default.MainContract.Elections(i);
+      var election = await new Election(electionAddress);
+      await election.init();
+      ElectionData.elections.push(election);
+    }
   },
 
   // This function will update the page with election details
   loadElectionDetails: async () => {
-      $('#electionDetails').html("");
-      for(var i = 0; i < ElectionData.electionCount; i++) {
-          var details = await ElectionData.elections[i].getDetails();
-          var votingForm;
-          // Showing voting forms to only non-voted elections
-          if(details.hasVoted) {
-              votingForm = `<td>
-                              <font size = 2 color = 'green'><b>Voted</b></font>
-                            </td>`
-          } else {
-              votingForm = `<td>
-                              <span>
-                                <input type='radio' name=${details.address} id="${details.address}0" onclick="ElectionData.elections[${i}].castVote(0)"> 
-                                <label for="${details.address}0"> ${details.candidates[0].name}</label>
-                        </span> <br>
-                              <span>
-                                <input type='radio' name=${details.address} id="${details.address}1" onclick="ElectionData.elections[${i}].castVote(1)"> 
-                                <label for="${details.address}1"> ${details.candidates[1].name}</label>
-                        </span>
-                            </td>`
-          }
-          var electionComponent = `<tr>
-                                      <td>${i}</td>
-                                      <td>${details.name}</td>
-                                      <td>
-                                          ${details.description}<br>
-                                          <font size = 2 class='text-muted'>
-                                              ${details.address}<br>
-                                              <b>${details.candidates[0].name} (${details.candidates[0].voteCount})</b> vs
-                                              <b>${details.candidates[1].name} (${details.candidates[1].voteCount})</b>
-                                          </font>
-                                      </td>
-                                      ${votingForm}
-                                  </tr>`
-          $('#electionDetails').append(electionComponent);
-      }
+    $("#elections").empty(); // Clear previous content
+    for (var i = 0; i < ElectionData.electionCount; i++) {
+      var details = await ElectionData.elections[i].getDetails();
+      var imagePath = `/assets/image${i + 1}.jpg`; // Image path
+      var votedMessage = details.hasVoted
+        ? `<div style="font-weight: bold; color: green;">You Voted</div>`
+        : "";
+
+      var electionComponent = `
+            <div class="election-section">
+                <b class="election-name">${details.name}</b>
+                <div class="row mt-3">
+                    <div class="col-md-4">
+                        <img src="${imagePath}" alt="Image for ${
+        details.name
+      }" class="img-fluid">
+                    </div>
+                    <div class="col-md-8">
+                        <p>${details.description}</p>
+                        <div> Current statistics: </div>
+                        <div class="text-muted">
+                            
+                            <b>${details.candidates[0].name} (${
+        details.candidates[0].voteCount
+      })</b> vs
+                            <b>${details.candidates[1].name} (${
+        details.candidates[1].voteCount
+      })</b>
+                        </div>
+                    </div>
+                </div>
+                ${votedMessage}
+                ${
+                  !details.hasVoted
+                    ? `
+                    <div >Choose your candidate:</div>
+                    <div>
+                        <input type='radio' name=${details.address} id="${details.address}0" onclick="ElectionData.elections[${i}].castVote(0)">
+                        <label for="${details.address}0">${details.candidates[0].name}</label><br>
+                        <input type='radio' name=${details.address} id="${details.address}1" onclick="ElectionData.elections[${i}].castVote(1)">
+                        <label for="${details.address}1">${details.candidates[1].name}</label>
+                    </div>`
+                    : ""
+                }
+            </div>`;
+      $("#elections").append(electionComponent);
+    }
   },
 
   // Function to create (deploy) election on the network
   createElection: async (details, candidates) => {
-      await Default.MainContract.createElection(details, candidates, {from: Default.account});
-      ElectionData.get();
-  }
-}  
+    await Default.MainContract.createElection(details, candidates, {
+      from: Default.account,
+    });
+    ElectionData.get();
+  },
+};
 
 window.ElectionData = ElectionData;
